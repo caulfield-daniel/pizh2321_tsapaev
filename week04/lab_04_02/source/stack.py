8f3cebdd81116244e9974acbb9dcf3ca134d08a5
Daniel Caulfield <danil.tsapaev@bk.ru>
2025-03-17 13:26:57 +0300

    feat: lab_04_02: class Stack: implement methods from_string, save, load
    Added and impemented methods of Stack class: from_string, save, load. main.py file corrected

--- week04/lab_04_02/source/stack.py
+++ week04/lab_04_02/source/stack.py
@@ -1,16 +1,25 @@
-class Stack:
-    """Класс, реализующий стек."""
+import json
 
+
+class Stack:
+    """
+    Класс, реализующий стек.
+    """
     def __init__(self):
-        """Инициализация стека."""
+        """
+        Инициализация стека.
+        """
         self.items = []
 
     def is_empty(self) -> bool:
-        """Проверяет, пуст ли стек."""
+        """
+        Проверяет, пуст ли стек.
+        """
         return len(self.items) == 0
 
     def push(self, item) -> None:
-        """Добавляет элемент на вершину стека.
+        """
+        Добавляет элемент на вершину стека.
 
         Параметры:
           - item: элемент, который нужно добавить в стек.
@@ -18,7 +27,8 @@ class Stack:
         self.items.append(item)
 
     def pop(self):
-        """Удаляет и возвращает элемент с вершины стека.
+        """
+        Удаляет и возвращает элемент с вершины стека.
 
         Возвращает:
           - элемент с вершины стека.
@@ -28,7 +38,8 @@ class Stack:
         return self.items.pop()
 
     def peek(self):
-        """Возвращает элемент с вершины стека без его удаления.
+        """
+        Возвращает элемент с вершины стека без его удаления.
 
         Возвращает:
           - элемент с вершины стека.
@@ -38,9 +49,41 @@ class Stack:
         return self.items[-1]
 
     def size(self) -> int:
-        """Возвращает количество элементов в стеке."""
+        """
+        Возвращает количество элементов в стеке.
+        """
         return len(self.items)
 
+    def from_string(self, string: str) -> None:
+        """Создает стек из строки.
+
+        Параметры:
+          - string: строка, содержащая элементы стека.
+        """
+        self.items = string.split()
+
+    def save(self, filename: str) -> None:
+        """
+        Сохраняет данные в файл в формате JSON.
+
+        Args:
+            filename (str): Имя файла, в который будут сохранены данные.
+        """
+        with open(filename, "w") as file:
+            json.dump({"stack": self.items}, file)
+
+    def load(self, filename: str) -> None:
+        """
+        Загружает данные из файла в формате JSON.
+
+        Args:
+            filename (str): Имя файла, из которого будут загружены данные.
+        """
+        with open(filename, "r") as file:
+            self.items = json.load(file)
+
     def __str__(self) -> str:
-        """Возвращает строковое представление стека."""
+        """
+        Возвращает строковое представление стека. (Начиная с вершины стека)
+        """
         return "Stack(" + ", ".join(repr(item) for item in reversed(self.items)) + ")"
